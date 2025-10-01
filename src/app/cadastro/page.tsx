7b4d88b64e23a57052e6212c79b708d8264f6987
Joyuv <givanilsonmariz7@outlook.com>
2025-10-01 11:49:28 -0300

    cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -3,14 +3,14 @@ import H1 from "@/app/ui/components/H1";
 import Link from "next/link";
 import Button from "@/app/ui/components/Button";
 
-// Trocar ancora por next Link
 export default function Page() {
   async function handleSubmit(formData: FormData) {
     "use server";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const username = formData.get("username") as string;
-    const res = await fetch("/api/auth/cadastro", {
+    const res = await fetch(`${baseUrl}/api/auth/cadastro`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -19,14 +19,20 @@ export default function Page() {
         username: username,
       }),
     });
+    return { sucess: true };
   }
   return (
     <div className={styles.centra}>
-      <form action={handleSubmit} className="flex flex-col gap-2">
+      <form
+        action={handleSubmit}
+        className={`${styles.form} rounded-xl flex flex-col gap-2`}
+      >
         <input type="email" name="email" placeholder="Email..." />
         <input type="password" name="password" placeholder="Senha..." />
         <input type="text" name="username" placeholder="Nome..." />
-        <button type="submit">Cadastrar</button>
+        <button className="rounded cursor-pointer" type="submit">
+          Cadastrar
+        </button>
       </form>
       <Link href="/" className="pointer">
         <Button>Voltar</Button>
